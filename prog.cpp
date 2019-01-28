4c5e1c47bc7d2b72828f7012fab625817238ca78
Kshitiz Saini <40308662+kshitizsaini113@users.noreply.github.com>
2019-01-28 17:41:21 +0530

    Added Switch Case

--- prog.cpp
+++ prog.cpp
@@ -4,5 +4,21 @@ using namespace std;
 
 int main()
 {
+  int choice;
+  cout<<"1. Addition"<<endl<<"2. Subtraction"<<endl<<"3. Multiplication"<<endl<<"4. Division"<<endl;
+  cout<<endl<<endl;
+  cout<<"Enter you choice : "<<endl;
+  cin>>choice;
+  switch(choice)
+  {
+    case 1:
+      cout<<"Addition";
+    case 2:
+      cout<<"Subtraction";
+    case 3:
+      cout<<"Multiplication";
+    case 4:
+      cout<<"Division";
+  }
   return 0;
 }
